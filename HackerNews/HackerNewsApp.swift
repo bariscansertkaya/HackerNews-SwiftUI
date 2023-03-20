b2adf06a4e3c99ea8c7d305c96b75bbcec70a266
Barış Can Sertkaya <bariscansertkaya2@gmail.com>
2023-03-20 17:41:23 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Barış Can Sertkaya on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
